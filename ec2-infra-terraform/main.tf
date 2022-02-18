5547a02b22cb35ee813ba7a6e85b40a68716f3bd
Aravind <aravindcse201820@gmail.com>
2022-02-18 19:16:41 +0530

    few changes in terraform and playbook

--- ec2-infra-terraform/main.tf
+++ ec2-infra-terraform/main.tf
@@ -66,15 +66,14 @@ resource "local_file" "ec2-dns" {
 }
 
 
+
 resource "null_resource" "execute_ansible_target" {
  
   depends_on = [aws_instance.ec2_instance, aws_cloudwatch_log_group.ec2_cwlogs]
 
   provisioner "local-exec" {
    command =  <<EOT
-      echo "Go and sleep for 180 seconds - Wait for completion of EC2 userdata!",
-      "sleep 180s",
-      echo "It's time to wake up !",
+      sleep 180s,
       export ANSIBLE_HOST_KEY_CHECKING=False, 
       ansible ${aws_instance.ec2_instance.public_dns} -u ec2-user --private-key ${local_file.ec2_private_key.filename} -m ping -i inventory.txt,
       ansible-playbook ../ansible-nginx-playbook/webserver_playbook.yml -u ec2-user --private-key ${local_file.ec2_private_key.filename} -i inventory.txt 
