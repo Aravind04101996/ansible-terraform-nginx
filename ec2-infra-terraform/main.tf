bfceb440336f60f4b1e351b973dd0358c3c5ba55
Aravind <aravindcse201820@gmail.com>
2022-02-18 11:47:18 +0530

    included ec2 creation terraform code

--- ec2-infra-terraform/main.tf
+++ ec2-infra-terraform/main.tf
@@ -46,6 +46,6 @@ resource "null_resource" "execute_ansible" {
   depends_on = [aws_instance.ec2_instance]
 
   provisioner "local-exec" {
-   command = "ansible -m ping -i inventory.txt"
+   command = "ansible ${aws_instance.ec2_instance.public_dns} -m ping -i inventory.txt"
   }
 }
